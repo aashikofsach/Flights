ead0357bc0b60b052c0e369a8576786d5723f076
aashish <ritikkamboj4314@gmail.com>
2026-04-10 15:34:14 +0530

    created middleware to check the modelNumber in request or not

--- src/routes/v1/airplane-router.js
+++ src/routes/v1/airplane-router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {airplaneController} = require("../../controllers")
+const {AirplaneMiddlewares} = require("../../middleware")
 
 const router = express.Router() ;
 
-router.post("/", airplaneController.createAirplane)
+router.post("/",AirplaneMiddlewares.validateCreateRequest, airplaneController.createAirplane)
 
 
 module.exports = router 
