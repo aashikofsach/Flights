1f5052fbb02e6c8f503d7512f4cc4330a98dff6c
aashish <ritikkamboj4314@gmail.com>
2026-04-20 23:44:02 +0530

    refactor: improve formatting and structure in airplane middleware; add airplane seeder

--- src/middleware/airplane-middleware.js
+++ src/middleware/airplane-middleware.js
@@ -1,19 +1,20 @@
 const { StatusCodes } = require("http-status-codes");
-const {ErrorResponse} = require("../utils/common");
+const { ErrorResponse } = require("../utils/common");
 const AppError = require("../utils/errors/app-error");
 
 function validateCreateRequest(req, res, next) {
-  
   if (!req.body.modelNumber) {
-      ErrorResponse.message = "Something went wrong, when we are creating request" ;
-    ErrorResponse.error = new AppError(['Model Number not found in the incoming request correctly'], StatusCodes.BAD_REQUEST)
+    ErrorResponse.message =
+      "Something went wrong, when we are creating request";
+    ErrorResponse.error = new AppError(
+      ["Model Number not found in the incoming request correctly"],
+      StatusCodes.BAD_REQUEST,
+    );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
-
 }
 
 module.exports = {
-    validateCreateRequest
-}
-
+  validateCreateRequest,
+};
