dcb454f9f14b3dce327cadbd66cfadeb2b70f74a
aashish <ritikkamboj4314@gmail.com>
2026-05-05 20:07:03 +0530

    refactor: remove error handling from CRUD operations for cleaner code

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -6,66 +6,40 @@ class CrudRepository {
   }
 
   async create(data) {
-    try {
-        console.log("body data in crud repo", data)
-      const response = await this.model.create(data);
-      console.log("what is the response ", response)
-      return response ;
-    } catch (error) {
-        console.log('error is yaha tak nakli', error )
-      logger.error("Something went wrong in the CRUD Repo : create");
-      throw error;
-    }
+    console.log("body data in crud repo", data);
+    const response = await this.model.create(data);
+    console.log("what is the response ", response);
+    return response;
   }
 
   async destroy(data) {
-    try {
-      const response = await this.model.destroy({
-        where: {
-          id: data,
-        },
-
-      });
-      return response
-    } catch (error) {
-      logger.error("Something went wrong in the CRUD Repo : create");
-      throw error;
-    }
+    const response = await this.model.destroy({
+      where: {
+        id: data,
+      },
+    });
+    return response;
   }
 
   async get(data) {
-    try {
-      const response = await this.model.findByPk(data);
-      return response ;
-    } catch (error) {
-      logger.error("Something went wrong in the CRUD Repo : create");
-      throw error;
-    }
+    const response = await this.model.findByPk(data);
+    return response;
   }
 
   async getAll(data) {
-    try {
-      const response = await this.model.findAll(data);
-      return response ;
-    } catch (error) {
-      logger.error("Something went wrong in the CRUD Repo : create");
-      throw error;
-    }
+    const response = await this.model.findAll(data);
+    return response;
   }
 
   async update(id, data) {
     // here data variable is object (having key and value pair )
-    try {
-      const response = await this.model.update(data, {
-        where: {
-          id: id,
-        },
-      });
-      return response ;
-    } catch (error) {
-      logger.error("Something went wrong in the CRUD Repo : create");
-      throw error;
-    }
+
+    const response = await this.model.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    return response;
   }
 }
 
