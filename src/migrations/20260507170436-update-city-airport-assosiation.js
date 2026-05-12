9f6c10c5e0d0f118bce8cfbfcb55bb9799225a1b
aashish <ritikkamboj4314@gmail.com>
2026-05-12 20:21:29 +0530

    feat: add foreign key constraint for cityId in Airports table ( async down ye to write )

---
+++ src/migrations/20260507170436-update-city-airport-assosiation.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint("Airports", "cityId", {
+      type: "foreign key",
+      references: {
+        model: "Cities",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
