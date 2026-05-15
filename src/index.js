2d3f8e1c0630b1fbf1395f5cf00a4e4c36588b5f
aashish <ritikkamboj4314@gmail.com>
2026-05-15 20:13:38 +0530

    feat : WIP on using helper functions from sequelize

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const {PORT} = require("./config");
-const express = require('express');
-const apiRoutes = require("./routes")
-const {ServerConfig , logger} = require("./config");
+const { PORT } = require("./config");
+const express = require("express");
+const apiRoutes = require("./routes");
+const { ServerConfig, logger } = require("./config");
 const db = require("./models");
 
-
-const app = express() ;
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRoutes)
+app.use("/api", apiRoutes);
 
+app.listen(ServerConfig.PORT, async () => {
+  console.log(`The server is running on the PORT: ${ServerConfig.PORT}`);
 
-
-app.listen(ServerConfig.PORT ,  () => {
-    console.log(`The server is running on the PORT: ${ServerConfig.PORT}`) ;
-   
-})
-
+  const { City, Airport } = require("./models");
+  const city = await City.findByPk(1);
+  console.log(city);
+  await city.createAirport({name : "Aashu's Airport" , address : "ladwa"})
+  const airports = await city.getAirports() ;
+  console.log(airports);
+});
