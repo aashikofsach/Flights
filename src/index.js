ebe164017eec9f8a34260fe2f331bf934917d1cd
aashish <ritikkamboj4314@gmail.com>
2026-05-18 20:02:06 +0530

    feat: create Flight model and migration with associations to Airports and Airplanes

--- src/index.js
+++ src/index.js
@@ -11,33 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT,  () => {
   console.log(`The server is running on the PORT: ${ServerConfig.PORT}`);
-
-  const { City, Airport } = require("./models");
-  // const city = await City.findByPk(1);
-  // console.log(city);
-  // await city.createAirport({name : "Aashu's Airport" , address : "ladwa"})
-  // const airports = await city.getAirports() ;
-  // console.log(airports);
-
-  // const aashu_airport = await Airport.findByPk(1);
-  // console.log(aashu_airport, "jai maata di ")
-  // await city.removeAirport(aashu_airport);
-
-  // await City.destroy({
-  //   where : {
-  //     id : 1
-  //   }
-  // })
-
-  // // await City.create({name : "Mumbai"});
-  // const city = await City.findByPk(10);
-  // console.log(city) ;
-
-  // // await city.createAirport({name : "Kempegowda" , address : "banglore", code : "KGD", cityId : 10});
-  // await city.createAirport({name :"Sach airport1", address : "banglore3" , code : "SATQ", cityId : 10})
-
 
 
 });
