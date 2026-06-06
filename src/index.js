93c04b90bd47c7e1455467dc233c98af8d06383c
aashish <ritikkamboj4314@gmail.com>
2026-06-06 14:13:23 +0530

    feat : applied middleware router for request coming from reverse proxy api gateway

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/flightservice/api",apiRoutes )
+
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT,  () => {
