a959908badd9345ae7c928694b9e27babfffbdd7
aashish <ritikkamboj4314@gmail.com>
2026-06-08 15:09:20 +0530

    refactor: used pathrewrite so no need for separate route path

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/flightservice/api",apiRoutes )
+// app.use("/flightservice/api",apiRoutes )
 
 app.use("/api", apiRoutes);
 
